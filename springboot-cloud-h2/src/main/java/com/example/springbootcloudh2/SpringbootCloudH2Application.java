eff2dec8ed596129a8a5101cb9995721a12fcefc
Hex0wn <8603870+hex0wn@users.noreply.github.com>
2020-09-10 19:08:09 +0800

    add Spring Boot Actuator RCE by Spring Cloud & H2 database

---
+++ springboot-cloud-h2/src/main/java/com/example/springbootcloudh2/SpringbootCloudH2Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootcloudh2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootCloudH2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootCloudH2Application.class, args);
+    }
+
+}
