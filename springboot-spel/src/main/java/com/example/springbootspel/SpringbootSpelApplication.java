c040b0e7031f34a4be8a6548b47575d66a6adefd
Hex0wn <8603870+hex0wn@users.noreply.github.com>
2020-09-10 14:54:18 +0800

    Spring Boot Whitelabel Error Page SpEL RCE

---
+++ springboot-spel/src/main/java/com/example/springbootspel/SpringbootSpelApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootspel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootSpelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootSpelApplication.class, args);
+    }
+
+}
