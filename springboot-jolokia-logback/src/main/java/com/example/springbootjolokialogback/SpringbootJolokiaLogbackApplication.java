bc6c9e9ee5d92f3c3d1879c1c94578c75deeaa55
admin <admin@github.com>
2020-09-10 15:45:41 +0800

    add Spring Boot Actuator RCE by jolokia & logback

---
+++ springboot-jolokia-logback/src/main/java/com/example/springbootjolokialogback/SpringbootJolokiaLogbackApplication.java
@@ -0,0 +1,13 @@
+package com.example.springbootjolokialogback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootJolokiaLogbackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootJolokiaLogbackApplication.class, args);
+    }
+
+}
